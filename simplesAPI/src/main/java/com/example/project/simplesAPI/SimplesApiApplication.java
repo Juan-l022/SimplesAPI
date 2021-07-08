3fae9ca8af94ce85adc80613f4738b4e78ccfdfc
Juan <juantec.lopes@gmail.com>
2021-07-08 20:19:12 -0300

    commit inicial

---
+++ simplesAPI/src/main/java/com/example/project/simplesAPI/SimplesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.project.simplesAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplesApiApplication.class, args);
+	}
+
+}
